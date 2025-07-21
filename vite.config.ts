cb2100ff841cce6bef80ec836c8ae35f02576f9b
atiarmridul <atiarmridul@gmail.com>
2025-07-22 05:46:22 +0600

    fixing config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-base: '/Portfolio/',
+  base: '/', // ✅ root domain — no subfolder
   plugins: [react()],
 });
